8f279137827ba8631dbd7f6d242722b20606bcd1
bobcarre Dragon <nicolas.zanin@etu.univ-cotedazur.fr>
2024-05-21 15:59:09 +0700

    feature: init server csharp #13

---
+++ Server_csharp_uplink/Controllers/PositionningController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Server_csharp_uplink.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PositionningController : ControllerBase
+    {
+        private readonly ILogger<PositionningController> _logger;
+
+        public PositionningController(ILogger<PositionningController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost(Name = "PositionSensor")]
+        public String Post()
+        {
+            return "Hello World\n";
+        }
+
+        [HttpGet(Name = "Start")]
+        public String Get()
+        {
+            return "Hello World\n";
+        }
+    }
+}
