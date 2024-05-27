7aad463366244a78dbf411dbc8c7fa152fe30748
NicolasZanin <nicolas.zanin@etu.univ-cotedazur.fr>
2024-05-27 16:39:49 +0700

    refactor: add last project in new solution #24
    add last project in new solution and rename this project with a better name

---
+++ application_c_sharp/api_csharp_uplink/Controllers/PositionningController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace api_csharp_uplink.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PositionningController : ControllerBase
+    {
+        private readonly ILogger<PositionningController> _logger;
+
+        public PositionningController(ILogger<PositionningController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet()]
+        public IActionResult timeBusToNextStation([FromQuery] int idStation)
+        {
+            if (idStation < 0)
+            {
+                return BadRequest("idStation must be a non-negative integer");
+            }
+
+            return Ok("5 mn");
+        }
+    }
+}
