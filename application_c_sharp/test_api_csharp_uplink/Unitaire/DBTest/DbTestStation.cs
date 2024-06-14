318346ee0536417ba7d7cd7437408635dd7556f9
NicolasZanin <nicolas.zanin@etu.univ-cotedazur.fr>
2024-06-14 14:36:18 +0700

    test: add test station #53
    add test to register Station and get Station By Name and position

---
+++ application_c_sharp/test_api_csharp_uplink/Unitaire/DBTest/DbTestStation.cs
@@ -0,0 +1,29 @@
+using api_csharp_uplink.Entities;
+using api_csharp_uplink.Interface;
+
+namespace test_api_csharp_uplink.Unitaire.DBTest;
+
+public class DbTestStation : IInfluxDbStation
+{
+    
+    private readonly List<Station> _context = [];
+    
+    public Task<Station> Add(Station station)
+    {
+        _context.Add(station);
+        return Task.FromResult(station);
+    }
+
+    public Task<Station?> GetStation(string nameStation)
+    {
+        Station? station = _context.Find(station => station.NameStation.Equals(nameStation));
+        return Task.FromResult(station);
+    }
+
+    public Task<Station?> GetStation(Position position)
+    {
+        Station? station = _context.Find(station => station.Position.Equals(position));
+        return Task.FromResult(station);
+    }
+    
+}
